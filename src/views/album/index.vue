0c022b31bf5c800bd7bd73b14a206694fa4a5e47
Abdul Mohaimen <mohaimen707@gmail.com>
2021-12-18 01:02:28 +0600

    remove commenting code

--- src/views/album/index.vue
+++ src/views/album/index.vue
@@ -1,37 +1,10 @@
-<!--<template>-->
-<!--  <div class="card my-3">-->
-<!--    <div class="card-header d-flex justify-content-between">-->
-<!--      <h4>Albums List</h4>-->
-<!--    </div>-->
-<!--    <div class="card-body table-responsive">-->
-<!--      <table class="table table-hover">-->
-<!--        <thead class="table-light">-->
-<!--        <tr>-->
-<!--          <th>Name</th>-->
-<!--          <th>Action</th>-->
-<!--        </tr>-->
-<!--        </thead>-->
-<!--        <tbody>-->
-<!--        <tr v-for="album in getAllAlbums" :key="album.id">-->
-<!--          <td>{{ album.title }}</td>-->
-<!--          <td class="d-flex justify-content-between">-->
-<!--            <router-link :to="`/albums/${album.id}`" class="btn btn-sm btn-info">View</router-link>-->
-<!--          </td>-->
-<!--        </tr>-->
-<!--        </tbody>-->
-<!--      </table>-->
-<!--    </div>-->
-<!--  </div>-->
-
-<!--</template>-->
-
 <template>
   <div class="row">
     <div class="col-md-8 offset-md-2 my-3">
       <div class="card">
         <div class="card-body">
           <h4>Albums List</h4>
-          <DataTable :value="getAllAlbums" :paginator="true" class="p-datatable-customers" :rows="4"
+          <DataTable :value="getAllAlbums" :paginator="true" class="p-datatable-customers" :rows="10"
                      dataKey="id" v-model:filters="albumFilters" filterDisplay="row" :loading="loading1" responsiveLayout="scroll"
           >
             <template #empty>
@@ -92,27 +65,3 @@ export default {
   }
 </script>
 
-<style >
-
-</style>
-
-
-<!--<script>
-  import {mapActions, mapState, mapGetters} from 'vuex'
-  export default {
-    name: "Albums",
-    components: {},
-    created() {
-      this.fetchAlbumsList();
-    },
-    methods: {
-      ...mapActions('albums', ['fetchAlbumsList']),
-    },
-    computed: {
-      ...mapState({
-        loading: state => state.user.loading
-      }),
-      ...mapGetters('albums', ['getAllAlbums'])
-    },
-  }
-</script>-->
