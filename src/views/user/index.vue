e231a2e25d2c5cf079f714d6941274a21325c57a
Abdul Mohaimen <mohaimen707@gmail.com>
2021-12-21 23:32:59 +0600

    remove pagination plugin codes

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -6,53 +6,15 @@
     </div>
     <div class="card-body table-responsive">
 
-      <DataTable :value="getAllUsers" :paginator="true" class="p-datatable-customers" :rows="4"
-                 dataKey="id" v-model:filters="userFilter" filterDisplay="row" :loading="loading1" responsiveLayout="scroll"
-      >
-        <template #empty>
-          No Data found.
-        </template>
-        <template #loading>
-          Loading data. Please wait.
-        </template>
-        <Column field="name" header="Name" style="min-width:12rem">
-          <template #body="{data}">
-            {{data.name}}
-          </template>
-          <template #filter="{filterModel,filterCallback}">
-            <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" placeholder="Search by name " />
-          </template>
-        </Column>
-        <Column field="email" header="Email" style="min-width:12rem">
-          <template #body="{data}">
-            {{data.email}}
-          </template>
-          <template #filter="{filterModel,filterCallback}">
-            <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" placeholder="Search by name " />
-          </template>
-        </Column>
-        <Column field="username" header="Username" style="min-width:12rem">
-          <template #body="{data}">
-            {{data.username}}
-          </template>
-          <template #filter="{filterModel,filterCallback}">
-            <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" placeholder="Search by name " />
-          </template>
-        </Column>
-        <Column header="Action" style="min-width:12rem">
-          <template #body="{data}">
-            <router-link :to="`/users/${data.id}`" class="btn btn-sm btn-info mr-5">
-              <i class="pi pi-eye" />
-            </router-link>
-            <button class="btn btn-sm btn-primary mr-5" @click="editUserHandler(data)">
-              <i class="pi pi-user-edit" />
-            </button>
-            <button class="btn btn-sm btn-danger" @click="onDelete(data.id)">
-              <i class="pi pi-trash" />
-            </button>
-          </template>
-        </Column>
-      </DataTable>
+<!--      <router-link :to="`/users/${data.id}`" class="btn btn-sm btn-info mr-5">-->
+<!--                      <i class="pi pi-eye" />-->
+<!--      </router-link>-->
+<!--      <button class="btn btn-sm btn-primary mr-5" @click="editUserHandler(data)">-->
+<!--        <i class="pi pi-user-edit" />-->
+<!--      </button>-->
+<!--      <button class="btn btn-sm btn-danger" @click="onDelete(data.id)">-->
+<!--        <i class="pi pi-trash" />-->
+<!--      </button>-->
     </div>
   </div>
   <UserAddModal
@@ -66,7 +28,6 @@
 <script>
   import {mapActions, mapState, mapGetters} from 'vuex'
   import UserAddModal from "@/components/user/UserAddModal";
-  import {FilterMatchMode} from "primevue/api";
   export default {
     name: "UserList",
     components: {UserAddModal},
@@ -75,20 +36,9 @@
         visible: false,
         editable: false,
         selectedUser: {},
-        userFilter: {
-          'name': {value: null, matchMode: FilterMatchMode.CONTAINS},
-          'email': {value: null, matchMode: FilterMatchMode.STARTS_WITH},
-          'username': {value: null, matchMode: FilterMatchMode.STARTS_WITH},
-        },
-        loading1: true
       }
     },
-    created() {
-      this.fetchUsersList();
-    },
-    mounted() {
-      this.loading1 = false;
-    },
+
     methods: {
       ...mapActions('user', ['fetchUsersList', 'deleteUser']),
       openModal(editable = false) {
